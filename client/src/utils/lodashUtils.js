2399a0b815f703377efca03a93588754dd5b5bf5
Tejas-CureFit <tejas.upmanyu@v.curefit.com>
2019-08-03 00:12:04 +0530

    base setup for workplace

---
+++ client/src/utils/lodashUtils.js
@@ -0,0 +1,7 @@
+import cloneDeep from 'lodash/cloneDeep';
+import get from 'lodash/get';
+import isEmpty from 'lodash/isEmpty';
+import isEqual from 'lodash/isEqual';
+import map from 'lodash/map';
+import capitalize from 'lodash/capitalize';
+export default { get, isEmpty, isEqual, cloneDeep, map, capitalize };
