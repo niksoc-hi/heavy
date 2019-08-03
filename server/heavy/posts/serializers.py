a33ccdb3faf68ba0936d6aa9c171c12604d5fec5
Dharanidhar Reddy <dharanidhar.reddy@hashedin.com>
2019-08-03 12:54:19 +0530

    Added Humanize

--- server/heavy/posts/serializers.py
+++ server/heavy/posts/serializers.py
@@ -1,5 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post, Comment
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+User = get_user_model()
+
+
+class Humanize:
+
+    def get_created_on(self, instance):
+        return naturaltime(instance.created_on)
+
+    def get_updated_at(self, instance):
+        return naturaltime(instance.updated_at)
 
 
 class VoteField:
@@ -9,9 +22,18 @@ class VoteField:
         return vote.action if vote else None
 
 
-class PostSerializer(VoteField, serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "username"]
+
+
+class PostSerializer(VoteField, Humanize, serializers.ModelSerializer):
     tags = serializers.ListField(source="tags.names", allow_empty=False)
     vote = serializers.SerializerMethodField()
+    user = UserSerializer()
+    created_on = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     class Meta:
         fields = "__all__"
@@ -32,8 +54,11 @@ class PostSerializer(VoteField, serializers.ModelSerializer):
         return modified_instance
 
 
-class CommentSerializer(VoteField, serializers.ModelSerializer):
+class CommentSerializer(VoteField, Humanize, serializers.ModelSerializer):
     vote = serializers.SerializerMethodField()
+    user = UserSerializer()
+    created_on = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
