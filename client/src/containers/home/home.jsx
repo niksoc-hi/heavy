06330bca99c893d85335b75d297b3fe18f2f8d5f
Tejas-CureFit <tejas.upmanyu@v.curefit.com>
2019-08-03 13:02:28 +0530

    API integrations for home

--- client/src/containers/home/home.jsx
+++ client/src/containers/home/home.jsx
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Card, Typography, Button, Tag } from 'antd'
 import './home.styles.scss'
 import user from '../../assets/images/user.png'
 import Helpers from '../../utils/helpers'
+import _ from '../../utils/lodashUtils'
 import { navigateToUrl } from '../../utils/navigationUtils'
 import { Post } from '../../components/post/post'
 import NewPost from '../../components/newPost/newPost'
+import { getAllPosts } from '../../actions/postActions'
 
 class Home extends Component {
+  componentDidMount() {
+    this.props.getAllPosts()
+  }
   navigateToPostDetails = postId => {
     navigateToUrl(`/posts/${postId}`)
   }
 
   renderPost = post => {
-    return <Post clickable onClick={this.navigateToPostDetails} />
+    return <Post clickable onClick={this.navigateToPostDetails} data={post} />
   }
 
   render() {
+    const { allPosts } = this.props
     return (
       <div className="home-container">
         <NewPost />
-        {[1, 2, 3].map(() => this.renderPost())}
+        {allPosts.map(post => this.renderPost(post))}
       </div>
     )
   }
 }
 
-export default Home
+const mapStateToProps = state => ({
+  allPosts: _.get(state, 'posts.allPosts.results', []),
+})
+const mapDispatchToProps = dispatch => ({
+  getAllPosts: () => dispatch(getAllPosts()),
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'
 import { activityIndicatorReducer } from './activityIndicatorReducer'
+import { postsReducer } from './postReducer'
 export default combineReducers({
   activityIndicator: activityIndicatorReducer,
+  posts: postsReducer,
 })
